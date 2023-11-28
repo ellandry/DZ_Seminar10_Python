1ecec65801f19ed234e57e2fbaf7123d7715e607
ellandry <ellandry777@mail.ru>
2023-11-28 19:02:23 +0300

    changed file with dz_10

--- dz_task44.py
+++ dz_task44.py
@@ -3,6 +3,7 @@
 # в one hot вид. Сможете ли вы это сделать без get_dummies?
 
 import pandas as pd 
+import numpy as np
 import random
  
 lst = ['robot'] * 10
@@ -10,5 +11,9 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
 data.head()
+
+#Создаем новые столбцы с помощью двоичного кодирования:
+data["robot"] = np.where(data["whoAmI"].str.contains("robot"), 1, 0)
+data["human"] = np.where(data["whoAmI"].str.contains("human"), 1, 0)
 
 print(data)
